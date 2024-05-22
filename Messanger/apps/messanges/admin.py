af1f4ee019c33839e430b5070752e596b676664e
Keks338 <kbochag@gmail.com>
2024-05-22 16:08:35 +0500

    Обновил модели с пользователями

--- Messanger/apps/messanges/admin.py
+++ Messanger/apps/messanges/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import MessageText
+from .models import MessageText, GroupChat
 
 @admin.register(MessageText)
 class MessageTextAdmin(admin.ModelAdmin):
     list_display = ('Text', 'Message_Creation_Date', 'Message_Edit_Date')
     search_fields = ('Sender_Id', 'Receiver_Id', 'Message_Creation_Date', 'Message_Edit_Date')
+
+@admin.register(GroupChat)
+class GroupChatAdmin(admin.ModelAdmin):
+    list_display = ('Group_name', 'Group_desc', 'Users_list_id', 'Users_admin_list_id')
