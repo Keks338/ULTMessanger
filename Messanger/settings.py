30f3171aa5476b419bebe732f8e7ae73c1549c4b
Keks338 <kbochag@gmail.com>
2024-06-06 08:43:39 +0500

    Commit before pull

--- Messanger/settings.py
+++ Messanger/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7u!q@77v%0(igz3zq@#l!g-gyy+ljftm6k$!(4(!*3mna5%gfn
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    # '192.168.201.17',
+    # '192.168.201.17'
     # '192.168.0.104'
 ]
 
